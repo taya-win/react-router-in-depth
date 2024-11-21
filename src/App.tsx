20e57cef38374879c24f667cea12e701aa272076
taya-win <tayawin.tyw@gmail.com>
2024-11-21 20:05:36 +0630

    React Router Basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+
 export default function App() {
-    return "React Router In Depth";
+    return <BrowserRouter>
+        <header>
+            <nav>
+                <h1>Jobarouter</h1>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+            </nav>
+
+        </header>
+        <Routes>
+            <Route index element={<Home />} />
+            <Route path='/about' element={<About />} />
+        </Routes>
+    </BrowserRouter>
 }
